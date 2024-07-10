a34649559e10bc0e4e624ffe928dce9b6654e9f4
xtus17 <cremagarrita@hotmail.com>
2024-07-09 23:10:13 -0500

    six

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0ZOredbG89TmqVz7X0P8FOWzUF08HtQ0",
-  authDomain: "empower-fec18.firebaseapp.com",
-  databaseURL: "https://empower-fec18-default-rtdb.firebaseio.com",
-  projectId: "empower-fec18",
-  storageBucket: "empower-fec18.appspot.com",
-  messagingSenderId: "611045930198",
-  appId: "1:611045930198:web:9f17c2af70142cc400c577"
+  apiKey: "AIzaSyA9J5qkri2pj8BABvxz5R4KeygZMWJmEjo",
+  authDomain: "empoderadasapp.firebaseapp.com",
+  databaseURL: "https://empoderadasapp-default-rtdb.firebaseio.com",
+  projectId: "empoderadasapp",
+  storageBucket: "empoderadasapp.appspot.com",
+  messagingSenderId: "183551164272",
+  appId: "1:183551164272:web:9090ffd2e3a14e1912d9b5"
 };
 
 export const Firebase = firebase.initializeApp(firebaseConfig);
